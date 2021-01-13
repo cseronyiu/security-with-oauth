fc2a8bfb2bd7ae42d8bfc237ef0ec2698dbeb3dc
Khairuzzaman ROnny <cse.rony.iu@gmail.com>
2021-01-13 08:21:09 +0600

    initial commit

---
+++ src/main/java/com/spring/securitywithoauth/SecurityWithOauthApplication.java
@@ -0,0 +1,15 @@
+package com.spring.securitywithoauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+@SpringBootApplication
+@EnableAuthorizationServer
+public class SecurityWithOauthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityWithOauthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/securitywithoauth/SecurityWithOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.securitywithoauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityWithOauthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
